d408b6dd521873c0eb28f86cefee93bb24075237
Md Azhar Ashraf <azhar.ashraf@milliman.com>
2023-03-11 14:50:42 +0530

    Add ApplicationDbContext

---
+++ AbbyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AbbyWeb.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbbyWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        private readonly ApplicationDbContext _context;
+        public ApplicationDbContext(ApplicationDbContext context)
+        {
+            _context= context;
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
